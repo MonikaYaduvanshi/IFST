a759f7f351d79b621c38fd17e9a337efe69e452e
priyanka <priyankagarg1698@gmail.com>
2020-01-04 14:03:12 +0530

    changes

--- app/src/main/java/com/example/ifst/HelpDesk.java
+++ app/src/main/java/com/example/ifst/HelpDesk.java
@@ -9,11 +9,11 @@ import android.os.Bundle;
 
 public class HelpDesk extends AppCompatActivity {
 
-    String[] venue = {"G-7", "G-7", "G-7", "G-7", "G-7", "G-7", "Auditorium", "Auditorium", "Auditorium", "Auditorium"};
+    String[] venue = {"G-7", "G-7", "G-7", "G-7", "G-7", "G-7", "Auditorium", "Auditorium", "Auditorium", "Auditorium","S-9","S-9"};
 
-    String[] name = {"Upma", "Pinky", "Pooja Chauhan", "Shivam Yadav", "Rohit Kumar", "Nadeem", "Dimple Singh", "Pritam", "Abhiniti Garg", "Sagun Sharma"};
+    String[] name = {"Upma", "Pinky", "Pooja Chauhan", "Shivam Yadav", "Rohit Kumar", "Nadeem", "Dimple Singh", "Pritam", "Abhiniti Garg", "Sagun Sharma","Pankaj Kumar","Khushi jhawar"};
 
-    String[] phone = {"+917082375866", "-", "+919816473187", "+918810500070", "+919518234798", "+919015320058", "+919958127533", "+919650332275", "+919999059703", "+917838839808"};
+    String[] phone = {"+917082375866", "-", "+919816473187", "+918810500070", "+919518234798", "+919015320058", "+919958127533", "+919650332275", "+919999059703", "+917838839808","+919050216441","+918287975969"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
